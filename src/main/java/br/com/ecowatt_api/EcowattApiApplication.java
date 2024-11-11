48a2de9aad84f1ba75edb9037851614d2ee9b610
freitaslivia <xliviafreitasx@gmail.com>
2024-11-11 17:44:53 -0300

    projeto criado

---
+++ src/main/java/br/com/ecowatt_api/EcowattApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecowatt_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcowattApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcowattApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ecowatt_api/EcowattApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecowatt_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcowattApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
